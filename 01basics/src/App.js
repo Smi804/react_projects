e72a7960ce6e334fccc0cce311b710f0860639c3
smi804 <abbasdanish804@gmail.com>
2024-11-14 11:50:20 +0500

    initial commit

---
+++ 01basics/src/App.js
@@ -0,0 +1,13 @@
+import New from "./new";
+
+function App() {
+  return (<>
+   <h1> Hi i AM SAMI</h1>
+   <New />
+
+  </>
+    
+  );
+}
+
+export default App;
---
+++ 01basics/src/new.js
@@ -0,0 +1,9 @@
+function New(){
+    return(
+        <>
+        <h1>Hello World</h1>
+        <p>A quick brown fox jumps over the lazy dog</p>
+        </>
+    )
+}
+export default New
