844c70e586297b842140dd95e1836a8de80a700a
smi804 <abbasdanish804@gmail.com>
2024-11-20 22:11:15 +0500

    code tag

--- miniContext/src/App.jsx
+++ miniContext/src/App.jsx
@@ -1,12 +1,13 @@
 import UserContextProvider from "./context/UserContextProvider"
 import Login from "./components/login"
 import Profile from "./components/Profile"
+import '../src/App.css'
 function App() {
  
 
   return (
    <UserContextProvider>
-   <h1>React UseContext project</h1>
+   <h1>React <code>UseContext </code> project</h1>
    <Login/>
    <Profile/>
    </UserContextProvider>
